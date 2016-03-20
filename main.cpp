491d7307012759b0150d71d6ca12a48ea2d5502c
DmitryLyukov <d.lyukov@ya.ru>
2016-03-20 04:00:39 +0300

    Reading of parameters was changed

--- main.cpp
+++ main.cpp
@@ -4,38 +4,43 @@
 
 #include "mandelbrot.hpp"
 
+
 int main(int argc, char* argv[]) {
     
+    //Default initialization
     int32_t width_screen  = 1280;
     int32_t height_screen =  720;
-    
-    if (argc > 2) {
-        width_screen  = atol(argv[1]);
-        height_screen = atol(argv[2]);
-    }
-    
     int palette = 3;
-    if (argc > 3) {
-        palette = atoi(argv[3]);
-    }
-    
     std::string path_to_file("mandelbrot.png");
-    if (argc > 4) {
-        path_to_file = argv[4];
-    }
-    
     bool progress_bar = false;
-    if (argc > 5) {
-        progress_bar  = atoi(argv[5]);
-    }
-    
     double x1 = -2., x2 = 1.;
     double y1 = -1., y2 = 1.;
-    if (argc > 9) {
-        x1 = atof(argv[6]);
-        x2 = atof(argv[7]);
-        y1 = atof(argv[8]);
-        y2 = atof(argv[9]);
+
+    for (int i = 1; i < argc; ++i) {
+        if (std::string (argv[i]) == std::string("-s")) {
+            width_screen  = atol(argv[++i]);
+            height_screen = atol(argv[++i]);
+            continue;
+        }
+        if (std::string (argv[i]) == std::string("-pal")) {
+            palette = atoi(argv[++i]);
+            continue;
+        }
+        if (std::string (argv[i]) == std::string("-f")) {
+            path_to_file = argv[++i];
+            continue;
+        }
+        if (std::string (argv[i]) == std::string("-bar")) {
+            progress_bar = true;
+            continue;
+        }
+        if (std::string (argv[i]) == std::string("-b")) {
+            x1 = atof(argv[++i]);
+            x2 = atof(argv[++i]);
+            y1 = atof(argv[++i]);
+            y2 = atof(argv[++i]);
+            continue;
+        }
     }
     
     painting(x1, x2, y1, y2, width_screen, height_screen, 
